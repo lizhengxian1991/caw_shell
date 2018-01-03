35dc9af240b88017b5811ca8d562b8828fd27582
zhengxian.lzx <lizhengxian1991@126.com>
2018-01-03 17:29:06 +0800

    导入头文件，使用cmake

--- jni/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE    := cawshell
-LOCAL_SRC_FILES := caw_shell.cpp
-
-include $(BUILD_SHARED_LIBRARY)
--- jni/Application.mk
+++
@@ -1,4 +0,0 @@
-APP_OPTIM := debug
-NDK_TOOLCHAIN := arm-linux-androideabi-4.7
-APP_ABI := armeabi-v7a
-APP_PLATFORM := android-19
